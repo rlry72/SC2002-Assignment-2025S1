7bc959afef424f8e07e3aefbc6833640be6d381f
rlry72 <lee.ryan1703@gmail.com>
2025-11-18 05:31:14 +0800

    more javadocs

--- model/Student.java
+++ model/Student.java
@@ -1,74 +1,100 @@
 package model;
-// import java.util.Scanner;
 
+/**
+ * student user entity that extends base user
+ */
 public class Student extends User {
-    private int yearOfStudy;
-    private String major;
-    private Internship internship;
 
+    /**
+     * year of study for student (1-4)
+     */
+    private int yearOfStudy;
+
+    /**
+     * major of student
+     */
+    private String major;
+
+
+    /**
+     * create student with id, name, email, year of study and major
+     * @param id unique id of student (e.g. U1234567A)
+     * @param name student name
+     * @param email student email
+     * @param year year of study (1 to 4)
+     * @param major student's major field
+     */
     public Student(String id, String name, String email, int year, String major) {
         super(id, name, email);
         yearOfStudy = year;
         this.major = major;
     }
 
-
+    /**
+     * get login id used by student when logging in
+     * @return student user id
+     */
+    @Override
     public String getLoginId() {
         return super.getUserId();
     }
 
+    /**
+     * get student's major
+     * @return major of student
+     */
     public String getMajor() {
         return major;
     }
 
+    /**
+     * get student's year of study
+     * @return year of study
+     */
     public int getYearOfStudy() {
         return yearOfStudy;
     }
 
-    @Override
-    public String getEmail() {
-        return super.getEmail();
-    }
-
-    @Override
-    public String getName() {
-        return super.getName();
-    }
-
-    @Override
-    public String getUserId() {
-        return super.getUserId();
-    }
-    
+    /**
+     * update student's email
+     * @param email new email value
+     */
     @Override
     public void setEmail(String email) {
         super.setEmail(email);
     }
 
+    /**
+     * update student's name
+     * @param name new name value
+     */
     @Override
     public void setName(String name) {
         super.setName(name);
     }
 
+    /**
+     * update student's user id
+     * @param userId new user id value
+     */
     @Override
     public void setUserId(String userId) {
         super.setUserId(userId);
     }
 
+    /**
+     * update student's major
+     * @param major new major value
+     */
     public void setMajor(String major) {
         this.major = major;
     }
 
+    /**
+     * update student's year of study
+     * @param yearOfStudy new year value
+     */
     public void setYearOfStudy(int yearOfStudy) {
         this.yearOfStudy = yearOfStudy;
     }
-
-    public Internship getInternship() {
-        return internship;
-    }
-
-    public void setInternship(Internship internship) {
-        this.internship = internship;
-    }
-    // apply for internship (max 3) -> need internship class?
 }
--- model/User.java
+++ model/User.java
@@ -1,62 +1,139 @@
 package model;
+
+/**
+ * abstract user entity representing all system users
+ */
 public abstract class User {
+
+    /**
+     * unique identifier of user
+     */
     private String userId;
+
+    /**
+     * name of user
+     */
     private String name;
+
+    /**
+     * password of user, default value is "password"
+     */
     private String password = "password";
+
+    /**
+     * email address of user
+     */
     private String email;
 
+    /**
+     * login state of user, true if logged in
+     */
     private boolean isLoggedIn = false;
 
+    /**
+     * create user with id, name and email
+     * @param id unique id for user
+     * @param n name of user
+     * @param email email address of user
+     */
     public User(String id, String n, String email) {
         userId = id;
         name = n;
         this.email = email;
     }
 
-
-    // methods - to be abstract?
+    /**
+     * log in user, sets login state to true
+     */
     public void login() {
         this.isLoggedIn = true;
-    };
+    }
 
+    /**
+     * log out user, sets login state to false
+     */
     public void logout() {
         isLoggedIn = false;
     }
 
+    /**
+     * get email of user
+     * @return email of user
+     */
     public String getEmail() {
         return email;
     }
+
+    /**
+     * get name of user
+     * @return name of user
+     */
     public String getName() {
         return name;
     }
-    // public String getPassword() {
-    //     return password;
-    // }
+
+    /**
+     * get user ID
+     * @return user ID
+     */
     public String getUserId() {
         return userId;
     }
+
+    /**
+     * update email of user
+     * @param email new email value
+     */
     public void setEmail(String email) {
         this.email = email;
     }
+
+    /**
+     * update name of user
+     * @param name new name value
+     */
     public void setName(String name) {
         this.name = name;
     }
+
+    /**
+     * update user ID
+     * @param userId new user ID value
+     */
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    /**
+     * check if user is currently logged in
+     * @return true if logged in, false otherwise
+     */
     public boolean isLoggedIn() {
         return isLoggedIn;
     }
 
+    /**
+     * manually set login state of user
+     * @param isLoggedIn new login state
+     */
     public void setLoggedIn(boolean isLoggedIn) {
         this.isLoggedIn = isLoggedIn;
     }
 
+    /**
+     * check if entered password matches stored password
+     * @param pw password to validate
+     * @return true if correct password, false otherwise
+     */
     public boolean validatePassword(String pw) {
         return this.password.equals(pw);
     }
 
+    /**
+     * change password if user is logged in
+     * @param newPw new password value
+     * @return true if password changed, false if user not logged in
+     */
     public boolean changePassword(String newPw) {
         if (!isLoggedIn)
             return false;
@@ -64,5 +141,9 @@ public abstract class User {
         return true;
     }
 
+    /**
+     * get value used for login (varies by user type)
+     * @return login ID string
+     */
     public abstract String getLoginId();
 }
